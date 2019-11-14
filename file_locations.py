4a055eb71a03487b9772599b84dd89851dcf8746
Jñani Crawford <crawford.jnani@gmail.com>
2019-11-13 20:30:54 -0800

    Cleaned base directory + Split notebooks for #2

---
+++ file_locations.py
@@ -0,0 +1,8 @@
+import socket
+PATH_TO_IMDB_GENDER = '[unknown]'
+
+if (socket.gethostname() == 'AMOXICILLIN'):
+	PATH_TO_IMDB_GENDER = 'Z:/IMDB_Gender/'
+
+if (socket.gethostname() == 'nyla'):
+	PATH_TO_IMDB_GENDER = '/biac2/kgs/projects/deepRFs/IMDB_Gender/ '
