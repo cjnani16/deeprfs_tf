9e8841d66e9b0ee7bc6d7091e887209df1de55b1
Jñani Crawford <crawford.jnani@gmail.com>
2019-11-15 14:19:04 -0800

    Added checkpoints folder to file_location.py, using that now in notebooks

--- file_locations.py
+++ file_locations.py
@@ -1,8 +1,11 @@
 import socket
 PATH_TO_IMDB_GENDER = '[unknown]'
+PATH_TO_CHECKPOINTS = '[unknown]'
 
 if (socket.gethostname() == 'AMOXICILLIN'):
 	PATH_TO_IMDB_GENDER = 'Z:/IMDB_Gender/'
+	PATH_TO_CHECKPOINTS = 'Z:/checkpoints_vgg16/'
 
 if (socket.gethostname() == 'nyla'):
-	PATH_TO_IMDB_GENDER = '/biac2/kgs/projects/deepRFs/IMDB_Gender/ '
+	PATH_TO_IMDB_GENDER = '/biac2/kgs/projects/deepRFs/IMDB_Gender/'
+	PATH_TO_CHECKPOINTS = '/biac2/kgs/projects/deepRFs/checkpoints_vgg16/'
