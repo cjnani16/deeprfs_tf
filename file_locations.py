10c71a8c6ff65fa164827b1c1e7bdd148cbe18e0
Jñani Crawford <crawford.jnani@gmail.com>
2020-01-24 13:55:39 -0800

    Update file locations for new datasets

--- file_locations.py
+++ file_locations.py
@@ -1,11 +1,17 @@
 import socket
-PATH_TO_IMDB_GENDER = '[unknown]'
+PATH_TO_IMDB_AGE = '[unknown]'
 PATH_TO_CHECKPOINTS = '[unknown]'
+PATH_TO_VPNL_ID = '[unknown]'
+PATH_TO_VPNL_GENDER = '[unknown]'
 
 if (socket.gethostname() == 'AMOXICILLIN'):
-	PATH_TO_IMDB_GENDER = 'Z:/IMDB_Gender/'
+	PATH_TO_IMDB_AGE = 'Z:/imdb_age/'
 	PATH_TO_CHECKPOINTS = 'Z:/checkpoints_vgg16/'
+	PATH_TO_VPNL_ID = 'Z:/FaceDataCp'
+	PATH_TO_VPNL_GENDER = 'Z:/FaceDataCp'
 
 if (socket.gethostname() == 'nyla' or socket.gethostname() == 'strelka'):
-	PATH_TO_IMDB_GENDER = '/biac2/kgs/projects/deepRFs/IMDB_Gender/'
+	PATH_TO_IMDB_AGE = '/biac2/kgs/projects/deepRFs/imdb_age/'
 	PATH_TO_CHECKPOINTS = '/biac2/kgs/projects/deepRFs/checkpoints_vgg16/'
+	PATH_TO_VPNL_ID = '/biac2/kgs/projects/deepRFs/FaceDataCp'
+	PATH_TO_VPNL_GENDER = '/biac2/kgs/projects/deepRFs/FaceDataCp'
