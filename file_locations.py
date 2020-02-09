9eb299ce010e656a2ac79b28512d73b16cf756a7
Jnani Crawford <jnani@strelka.stanford.edu>
2020-02-08 17:15:17 -0800

    update file locations with gender

--- file_locations.py
+++ file_locations.py
@@ -8,10 +8,10 @@ if (socket.gethostname() == 'AMOXICILLIN'):
 	PATH_TO_IMDB_AGE = 'Z:/imdb_age/'
 	PATH_TO_CHECKPOINTS = 'Z:/checkpoints_vgg16/'
 	PATH_TO_VPNL_ID = 'Z:/FaceDataCp'
-	PATH_TO_VPNL_GENDER = 'Z:/FaceDataCp'
+	PATH_TO_VPNL_GENDER = 'Z:/FaceDataGender'
 
 if (socket.gethostname() == 'nyla' or socket.gethostname() == 'strelka'):
 	PATH_TO_IMDB_AGE = '/biac2/kgs/projects/deepRFs/imdb_age/'
 	PATH_TO_CHECKPOINTS = '/biac2/kgs/projects/deepRFs/checkpoints_vgg16/'
 	PATH_TO_VPNL_ID = '/biac2/kgs/projects/deepRFs/FaceDataCp'
-	PATH_TO_VPNL_GENDER = '/biac2/kgs/projects/deepRFs/FaceDataCp'
+	PATH_TO_VPNL_GENDER = '/biac2/kgs/projects/deepRFs/FaceDataGender'
